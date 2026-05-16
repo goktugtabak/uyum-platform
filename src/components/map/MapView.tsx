e7e16294a99397ec865c9121e22ca3f231245e8d
goktugtabak <goktug.tabakk@gmail.com>
2026-05-16 20:29:37 +0300

    feat: add leaflet map view with hybrid pins

---
+++ src/components/map/MapView.tsx
@@ -0,0 +1,46 @@
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import { FacilityPin } from './FacilityPin'
+import { MapLegend } from './MapLegend'
+import type { Facility, DisabilityType } from '../../types'
+
+interface MapViewProps {
+  facilities:     Facility[]
+  disabilityType: DisabilityType
+  sportFilter:    string | null
+}
+
+const ANKARA_CENTER: [number, number] = [39.9334, 32.8597]
+
+export function MapView({ facilities, disabilityType, sportFilter }: MapViewProps) {
+  return (
+    <div className="relative h-full w-full min-h-[60vh] md:min-h-[calc(100vh-8rem)]">
+      <MapContainer
+        center={ANKARA_CENTER}
+        zoom={12}
+        scrollWheelZoom
+        className="h-full w-full"
+        style={{ minHeight: 'inherit' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {facilities.map(facility => {
+          const isHighlighted = sportFilter ? facility.sports.includes(sportFilter) : false
+          const isDimmed      = sportFilter ? !facility.sports.includes(sportFilter) : false
+          return (
+            <FacilityPin
+              key={facility.id}
+              facility={facility}
+              disabilityType={disabilityType}
+              isHighlighted={isHighlighted}
+              isDimmed={isDimmed}
+            />
+          )
+        })}
+      </MapContainer>
+      <MapLegend />
+    </div>
+  )
+}
