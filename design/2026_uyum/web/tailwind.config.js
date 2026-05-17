b74e6e392f41239797396b3bd56aa53bcfd5f680
berkekus <izzettinberke@gmail.com>
2026-05-17 03:01:15 +0300

    .

--- design/2026_uyum/web/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-const sharedConfig = require('../shared/tailwind.config')
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  ...sharedConfig,
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
-}
